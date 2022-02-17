e704825217a77d258b9670b3d54ca432e179e30b
Hani Mounla <hani.mounla@outlook.com>
2022-02-17 02:22:41 +0200

    Rasa API updates

--- django_rasa/core/views.py
+++ django_rasa/core/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+
+@csrf_exempt
+def webhook(request):
+	print(request.POST)
+	return JsonResponse({"status": "OK"})
