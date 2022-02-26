abec4de62cc84e3f7d08ceededfecc17a5ca9f1c
Hani Mounla <hani.mounla@outlook.com>
2022-02-27 01:37:52 +0200

    Added domain dynamic content

--- django_rasa/bot/admin.py
+++ django_rasa/bot/admin.py
@@ -5,8 +5,9 @@ from django.core.files.storage import default_storage
 from .models import QuestionCategory, Question, Answer
 
 
-def create_nlu():
-    nlu_file_path = 'data/nlu.yml'
+def create_bot_files():
+    nlu_file_path = 'rasa_files/data/nlu.yml'
+    domain_file_path = 'rasa_files/domain.yml'
     
     nlu_string = "version: \"3.0\" \n \nnlu: \n"
     for category in QuestionCategory.objects.all():
@@ -18,10 +19,30 @@ def create_nlu():
     if default_storage.exists(nlu_file_path):
         default_storage.delete(nlu_file_path)
     default_storage.save(nlu_file_path, ContentFile(nlu_string))
+    
+    domain_string = "version: \"3.0\" \n \nintents: \n"
+    for category in QuestionCategory.objects.all():
+        domain_string += f" - {category} \n"
+        
+    domain_string += "\n \nresponses: \n"
+    
+    for category in QuestionCategory.objects.all():
+        domain_string += f"     utter_{category}: \n"
+        for question in Question.objects.filter(question_category=category):
+            try:
+                domain_string += f"    - text: {Answer.objects.get(question=question)} \n"
+            except:
+                pass
+        domain_string += "\n \n"
+            
+    if default_storage.exists(domain_file_path):
+        default_storage.delete(domain_file_path)
+    default_storage.save(domain_file_path, ContentFile(domain_string))
+         
         
 class BotAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
-        create_nlu()
+        create_bot_files()
         super().save_model(request, obj, form, change)
     
 admin.site.register(QuestionCategory, BotAdmin)
